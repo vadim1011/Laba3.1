39b8da28edd4675513ca35ce41a23ddba6d3bb0d
vadim1011 <vadim.nik999@gmail.com>
2025-10-13 20:32:57 +0200

    Add files via upload

---
+++ Laba3.2.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите вещественное число X: ");
+double X = double.Parse(Console.ReadLine());
+Console.Write("Введите целое число N (> 0): ");
+int N = int.Parse(Console.ReadLine());
+if (N <= 0)
+{
+    Console.WriteLine("Ошибка: N должно быть больше 0.");
+    return;
+}
+double sum = 1.0;
+for (int k = 1; k <= N; k++)
+{
+    double term = Math.Pow(X, 2 * k) / (2 * k);
+    if (k % 2 == 1)
+        sum -= term;
+    else
+        sum += term;
+}
+Console.WriteLine($"Результат: {sum}");
---
+++ Laba3.3.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите значение x:");
+double x = double.Parse(Console.ReadLine());
+double t = (x - 1) / (x + 1);
+double sum = 0;
+
+for (int i = 1; i <= 5; i++)
+{
+    double stepen = 2 * i - 1;
+    double koeffitsient = 1.0 / stepen;
+    double chlen = koeffitsient * Math.Pow(t, stepen);
+    sum += chlen;
+    Console.WriteLine($"Член {i}: {chlen}");
+}
+Console.WriteLine($"Сумма ряда: {sum}");
+    
