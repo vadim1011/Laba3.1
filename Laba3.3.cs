481908953a5562607215dc9f5948d0a77c0bc6d0
vadim1011 <vadim.nik999@gmail.com>
2025-11-14 00:18:10 +0300

    Update Laba3.3.cs

--- Laba3.3.cs
+++ Laba3.3.cs
@@ -1,15 +1,22 @@
-﻿Console.Write("Введите значение x:");
+Console.Write("Введите значение x:");
 double x = double.Parse(Console.ReadLine());
 double t = (x - 1) / (x + 1);
 double sum = 0;
-
 for (int i = 1; i <= 5; i++)
 {
+    double chlen = 0;
     double stepen = 2 * i - 1;
-    double koeffitsient = 1.0 / stepen;
-    double chlen = koeffitsient * Math.Pow(t, stepen);
+    switch (stepen)
+    {
+        case double s when s > 0:
+            double koeffitsient = 1.0 / s;
+            chlen = koeffitsient * Math.Pow(t, s);
+            break;
+        default:
+            chlen = 0;
+            break;
+    }
     sum += chlen;
     Console.WriteLine($"Член {i}: {chlen}");
 }
 Console.WriteLine($"Сумма ряда: {sum}");
-    
